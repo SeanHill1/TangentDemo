be4b85f90c1b5c04b2b5b6afedebedca7f7f1cb6
Sean Hill <shill@retrorabbit.co.za>
2017-04-19 20:03:34 +0200

    basic login test and client

---
+++ TangentSolutionsProject/Client/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+using TangentSolutionsProject.Models;
+
+namespace TangentSolutionsProject.Clients
+{
+    public class Client
+    {
+        protected HttpClient client { get; private set; } = new HttpClient();
+        private String token = null;
+        public Client()
+        {
+
+        }
+        
+        public async Task<bool> getToken(LoginModel user)
+        {
+            
+            if (token != null)
+            {
+                return true;
+            }
+            Uri uri = new Uri("http://userservice.staging.tangentmicroservices.com:80/api-token-auth/");
+            try
+            {
+                var jsonUser = new JavaScriptSerializer().Serialize(user);
+                var httpContent = new StringContent(jsonUser, Encoding.UTF8, "application/json");
+
+                var httpResponse = await client.PostAsync(uri, httpContent);
+
+                if (httpResponse.Content != null)
+                {
+                    var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                    return true;
+                }
+                else throw new Exception("No response");
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+    }
+}
---
+++ TangentSolutionsProjectTests/LoginTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TangentSolutionsProject.Models;
+using TangentSolutionsProject.Clients;
+using System.Threading.Tasks;
+
+namespace TangentSolutionsProjectTests
+{
+    /// <summary>
+    /// Test login
+    /// </summary>
+    [TestClass]
+    public class LoginTests
+    {
+        Client client = new Client(); 
+        public LoginTests()
+        {
+        }
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void testLogin()
+        {
+            var result = client.getToken(
+                    new TangentSolutionsProject.Models.LoginModel()
+                    { username = "jacob.zuma",
+                        password = "tangent"
+                    });
+            Task.WaitAll(result);
+            
+            Assert.IsTrue(result.Result);
+        }
+    }
+}
