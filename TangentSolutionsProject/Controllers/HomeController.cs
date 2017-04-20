0b2b14a43a916de71af4b08591d1436cece5c1bb
Sean Hill <shill@retrorabbit.co.za>
2017-04-20 18:03:27 +0200

    way pre-styling table

--- TangentSolutionsProject/Controllers/HomeController.cs
+++ TangentSolutionsProject/Controllers/HomeController.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TangentSolutionsProject.Clients;
+using TangentSolutionsProject.Models;
 
 namespace TangentSolutionsProject.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            if (Session["token"] == null)
+                return RedirectToAction("Login", "Login");
+
+            Client client = new Client();
+            
+            string token = (string)Session["token"];
+
+            var projects = await client.getProjects(token);
+
+            return View(projects);
         }
     }
 }
